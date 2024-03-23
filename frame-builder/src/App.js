69cbf8599b90f12d4a09bfcd71ad3901d55ca802
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-03-23 14:52:55 -0500

    File upload system to upload pdf

--- frame-builder/src/App.js
+++ frame-builder/src/App.js
@@ -33,6 +33,7 @@ function App() {
   const [movementDescription, setMovementDescription] = useState('');
   const [backgroundColor, setBackgroundColor] = useState('#96b7ff');
   const [donationAddress, setDonationAddress] = useState('');
+  const [selectedPDF, setSelectedPDF] = useState([]);
 
   const handleTitleChange = (e) => {
     setMovementTitle(e.target.value);
@@ -51,6 +52,7 @@ function App() {
     if (!divToConvert) return;
 
     let downloadURL;
+    let pdfUrl;
 
     try {
       const blob = await toBlob(divToConvert, { pixelRatio: 2 });
@@ -62,12 +64,23 @@ function App() {
       console.log('Uploaded a blob to Firebase Storage');
       downloadURL = await getDownloadURL(fileRef);
       console.log('Download URL:', downloadURL);
+
+      //also uploda the pdf
+      const fileRef2 = ref(storage, (id+1).toString()); 
+      await uploadBytes(fileRef2, selectedPDF[0]);
+      pdfUrl = await getDownloadURL(fileRef2);
+
     } catch (error) {
       console.error('Error uploading blob to Firebase Storage:', error);
       return null;
     }
 
-    return downloadURL;
+    return {downloadURL, pdfUrl};
+  };
+
+  const handlePDFUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedPDF(file);
   };
 
   const handleSubmit = () => {
@@ -92,7 +105,7 @@ function App() {
       });
     }
 
-    if (!(movementTitle && movementDescription && donationAddress)) {
+    if (!(movementTitle && movementDescription && donationAddress && selectedPDF.length != 0)) {
       toast.error('Ensure fields not left blank!', {
         position: "top-right",
         autoClose: 5000,
@@ -108,9 +121,9 @@ function App() {
 
     var returnID;
 
-    handleConvertToImage().then(function(result){
+    handleConvertToImage().then(function({downloadURL, pdfUrl}){
 
-      if(result == null){
+      if(downloadURL == null || pdfUrl == null){
         toast.error('Error uploading Movement!', {
           position: "top-right",
           autoClose: 5000,
@@ -124,11 +137,10 @@ function App() {
         return;
       }
       else {
-        returnID = result;
         console.log(" ABOUT TO WRITE TO CONTRACT .. ")
 
         write({
-          args: [movementTitle, returnID, returnID],
+          args: [movementTitle, pdfUrl, downloadURL],
           onError(error) {
             console.log('Error', error)
           },
@@ -145,9 +157,9 @@ function App() {
 
       }
 
-
-      console.log(result + "result") 
-      return result;
+      //console.log(result + "result") 
+      //return result;
+      return;
 
     });
     
@@ -197,12 +209,21 @@ function App() {
               <HexColorPicker color={backgroundColor} onChange={setBackgroundColor} className='mb-4 w-32' />
             </div>
 
-            <input
+            {/* <input
               type="text"
               placeholder="What address should donations be sent to?"
               className="border border-gray-300 rounded px-4 py-2 mb-4 w-full"
               value={donationAddress}
               onChange={handleDonationAddressChange}
+            /> */}
+            
+            <h1 className='text-gray-400'> Where can signers learn about what they are signing?</h1>
+            <input
+              className="border border-gray-300 rounded px-4 py-2 mb-4 w-full mt-4"
+              type="file"
+              accept=".pdf"
+              onChange={handlePDFUpload}
+              placeholder='Upload Contract PDF'
             />
 
             <button
@@ -212,12 +233,6 @@ function App() {
               Submit
             </button>
             
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
-              onClick={handleConvertToImage}
-            >
-              test button
-            </button>
 
           </div>
         </div>
