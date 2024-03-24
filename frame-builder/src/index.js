1cb16c3555e951d90bf290e7b664202d00890076
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-03-23 23:01:43 -0500

    reverted breaking changes from alchemy

--- frame-builder/src/index.js
+++ frame-builder/src/index.js
@@ -11,7 +11,7 @@ import {
 } from "@dynamic-labs/sdk-react-core";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import {ZeroDevSmartWalletConnectors} from "@dynamic-labs/ethereum-aa"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,7 +20,7 @@ root.render(
     settings={{
       // Find your environment id at https://app.dynamic.xyz/dashboard/developer
       environmentId: '0ca24247-9679-4abc-8963-bb5f36ad358b',
-      walletConnectors: [EthereumWalletConnectors, ZeroDevSmartWalletConnectors],
+      walletConnectors: [EthereumWalletConnectors],
     }}
     >
       <DynamicWagmiConnector>
